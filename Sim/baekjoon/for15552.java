377105507b9d4d16c09364325482537d5f06ec1b
Nonspecialist1 <dkeka1187@naver.com>
2021-11-16 10:40:40 +0900

    baekjoon

---
+++ Sim/baekjoon/for15552.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.io.*;
+
+public class for15552 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        System.out.println(line);
+
+    }
+}
